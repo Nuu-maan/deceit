12a220587e47dd347ad2d59f69f1af442047b2be
Nuu-maan <n82238895@gmail.com>
2024-09-25 08:36:57 +0530

    Added sexy userinfo command ( beta )

---
+++ commands/info/userinfo.js
@@ -0,0 +1,55 @@
+const { EmbedBuilder } = require('discord.js');
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const { EMBED_COLOR } = require('../../constants');
+
+module.exports = {
+    name: 'ui',
+    description: 'Get user information by user ID or mention.',
+    async execute(message, args) {
+        const userId = message.mentions.users.first()?.id || args[0];
+
+        if (!userId) {
+            return message.reply('Please provide a valid user ID or mention.');
+        }
+
+        try {
+            // Fetch user data from the API
+            const userResponse = await fetch(`https://dcdn.n0step.xyz/users/${userId}`);
+            const userData = await userResponse.json();
+
+            // Fetch user profile data from the API
+            const profileResponse = await fetch(`https://dcdn.n0step.xyz/profile/${userId}`);
+            const profileData = await profileResponse.json();
+
+            const avatarUrl = `https://cdn.discordapp.com/avatars/${userId}/${userData.avatar}.png`;
+            const bannerUrl = profileData.user.banner
+                ? `https://cdn.discordapp.com/banners/${userId}/${profileData.user.banner}.png`
+                : 'None';
+
+            const connectedAccounts = profileData.connected_accounts.length > 0
+                ? profileData.connected_accounts.map(account => `${account.type}: \`${account.name}\``).join('\n')
+                : 'None';
+
+            const embed = new EmbedBuilder()
+                .setColor(EMBED_COLOR)
+                .setTitle(`${userData.username}#${userData.discriminator}`)
+                .setThumbnail(avatarUrl)
+                .addFields(
+                    { name: 'ID', value: userData.id, inline: true },
+                    { name: 'Avatar', value: `[Link](${avatarUrl})`, inline: true },
+                    { name: 'Banner', value: bannerUrl !== 'None' ? `[Link](${bannerUrl})` : 'None', inline: true },
+                    { name: 'Global Name', value: userData.global_name || 'None', inline: true },
+                    { name: 'Bio', value: profileData.user_profile.bio || 'None', inline: true }
+                )
+                .addFields({ name: 'Connected Accounts', value: connectedAccounts })
+                .setFooter({ text: 'Powered by n0step.xyz', iconURL: 'https://example.com/icon.png' })
+                .setTimestamp();
+
+            return message.channel.send({ embeds: [embed] });
+        } catch (error) {
+            console.error('Error fetching user info:', error);
+            return message.reply('An error occurred while fetching user information. Please try again later.');
+        }
+    }
+};
